2e20e86cb2a14694da7d65d6ae57447d6b877b5d
cleithoff <carsten.leithoff@cu-medien.com>
2014-09-13 10:50:24 +0200

    timezone

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -9,6 +9,7 @@ class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
 	
 	protected function _initConfig()
 	{
+		date_default_timezone_set('Europe/Berlin');
 		$config = new Zend_Config($this->getOptions(), true);
 		Zend_Registry::set('config', $config);
 		return $config;
