a6d9d1d4c81ab0fc42460eec2cfbe6bfd02105ce
cleithoff <carsten.leithoff@cu-medien.com>
2014-09-14 22:10:12 +0200

    font conf

--- library/Rest/Pdf.php
+++ library/Rest/Pdf.php
@@ -4,7 +4,8 @@ class Rest_Pdf {
 	
 	public static function fop($xml, $xsl, $pdf) {
 		$fop = APPLICATION_PATH . Zend_Registry::getInstance()->config->cli->fop;
-		$exec = $fop . ' -c conf/userconfig.xml -xml ' . $xml . ' -xsl ' . $xsl . ' -pdf ' . $pdf;
+		echo $exec = $fop . ' -c conf/userconfig.xml -xml ' . $xml . ' -xsl ' . $xsl . ' -pdf ' . $pdf;
+		die();
 		exec($exec);
 	}
 	
