744a7a42e824a415777d821beb84be3da8412d43
cleithoff <carsten.leithoff@cu-medien.com>
2014-10-24 14:06:53 +0200

    DMA-69

--- application/modules/product/services/Plugingutscheincardplakata3.php
+++ application/modules/product/services/Plugingutscheincardplakata3.php
@@ -10,7 +10,7 @@ class Product_Service_Plugingutscheincardplakata3 extends Product_Service_Plugin
 	public function postProcess(Order_Model_Item $order_item, $viewmode, $resourcePdfFile, $publicDeployFile) {
 		switch($viewmode) {
 			case Product_Model_Layout::VIEW_PREVIEW_FRONT:
-				$exec = 'convert -density 96 "' . $resourcePdfFile . '"  -quality 80 -resize 50% "' . $publicDeployFile . '"';
+				$exec = 'convert -density 96 "' . $resourcePdfFile . '"  -quality 80 "' . $publicDeployFile . '"';
 				// echo $exec;die();
 				exec($exec);
 				break;
