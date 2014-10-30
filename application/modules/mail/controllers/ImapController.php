fa4cb0104768b2e01a345fc2ddf43616189ebc2e
cleithoff <carsten.leithoff@cu-medien.com>
2014-10-30 15:01:01 +0100

    bugfix

--- application/modules/mail/controllers/ImapController.php
+++ application/modules/mail/controllers/ImapController.php
@@ -58,6 +58,106 @@ class Mail_ImapController extends Zend_Controller_Action
     	}
     	return $data;
     }
+    
+    protected function getMails(Zend_Mail_Storage_Imap $storage, $filter) {
+    	$partner_nr = reset($filter);
+    	// if (empty($partner_nr)) $partner_nr = "99999999";
+    	$partner_nr = $partner_nr->value;
+    	if (empty($partner_nr)) return; // $this->getRequest()->getParam('partner_nr', "00000000");
+    	$messages = $storage->search($partner_nr);
+    	
+    	$data = array();
+    	foreach ($messages as $messageid) {
+    		$datamsg = new stdClass();
+    		$msg = $storage->getMessage($messageid);
+    		$datamsg->id = $messageid;
+    		$datamsg->mailFrom = iconv("ISO-8859-1", "UTF-8", quoted_printable_decode($msg->getHeaderField('from')));
+    		$datamsg->mailTo = iconv("ISO-8859-1", "UTF-8", quoted_printable_decode($msg->getHeaderField('to')));
+    		$datamsg->mailSubject = iconv("ISO-8859-1", "UTF-8", quoted_printable_decode($msg->getHeaderField('subject')));
+    		$datamsg->mailDate = date('Y-m-d H:i:s', strtotime($msg->getHeaderField('date')));
+    		$datamsg->mailMessage = "";
+
+    		/*
+    		$foundPart = null;
+    		foreach (new RecursiveIteratorIterator($msg) as $part) {
+    			try {
+    	
+    				if (strtok($part->contentType, ';') == 'text/plain' || strtok($part->contentType, ';') == 'text/html') {
+    					$foundPart = $part;
+    					break;
+    				}
+    			} catch (Zend_Mail_Exception $e) {
+    				// ignorieren
+    			}
+    		}
+    		if (empty($foundPart)) {
+    			$datamsg->mailMessage = iconv("ISO-8859-1", "UTF-8", strip_tags(quoted_printable_decode($msg->getContent())));
+    		} else {
+    			$datamsg->mailMessage = iconv("ISO-8859-1", "UTF-8", strip_tags(quoted_printable_decode($foundPart)));
+    		}
+    		 
+    		$testc = new stdClass();
+    		$testc->test = utf8_decode($datamsg->mailMessage);
+    		$testc = json_decode(json_encode($testc));
+    		if (!empty($testc->test)) {
+    			$datamsg->mailMessage = utf8_decode($datamsg->mailMessage);
+    		}
+    		*/
+    			
+    		 
+    		$data[] = $datamsg;
+    	}
+    	return $data;
+    }
+    
+    protected function getMail(Zend_Mail_Storage_Imap $storage, $messageid) {
+    	// if (empty($partner_nr)) $partner_nr = "99999999";
+    	$messageid = $messageid->value;
+    	if (empty($messageid)) return; // $this->getRequest()->getParam('partner_nr', "00000000");
+    	
+    	$data = array();
+
+    		$datamsg = new stdClass();
+    		$msg = $storage->getMessage($messageid);
+    		$datamsg->id = $messageid;
+    		$datamsg->mailFrom = iconv("ISO-8859-1", "UTF-8", quoted_printable_decode($msg->getHeaderField('from')));
+    		$datamsg->mailTo = iconv("ISO-8859-1", "UTF-8", quoted_printable_decode($msg->getHeaderField('to')));
+    		$datamsg->mailSubject = iconv("ISO-8859-1", "UTF-8", quoted_printable_decode($msg->getHeaderField('subject')));
+    		$datamsg->mailDate = date('Y-m-d H:i:s', strtotime($msg->getHeaderField('date')));
+    		$datamsg->mailMessage = "";
+
+    		
+    		$foundPart = null;
+    		foreach (new RecursiveIteratorIterator($msg) as $part) {
+    			try {
+    	
+    				if (strtok($part->contentType, ';') == 'text/plain' || strtok($part->contentType, ';') == 'text/html') {
+    					$foundPart = $part;
+    					break;
+    				}
+    			} catch (Zend_Mail_Exception $e) {
+    				// ignorieren
+    			}
+    		}
+    		if (empty($foundPart)) {
+    			$datamsg->mailMessage = iconv("ISO-8859-1", "UTF-8", strip_tags(quoted_printable_decode($msg->getContent())));
+    		} else {
+    			$datamsg->mailMessage = iconv("ISO-8859-1", "UTF-8", strip_tags(quoted_printable_decode($foundPart)));
+    		}
+    		 
+    		$testc = new stdClass();
+    		$testc->test = utf8_decode($datamsg->mailMessage);
+    		$testc = json_decode(json_encode($testc));
+    		if (!empty($testc->test)) {
+    			$datamsg->mailMessage = utf8_decode($datamsg->mailMessage);
+    		}
+    		
+    			
+    		 
+    		$data[] = $datamsg;
+
+    	return $data;
+    }
 
     public function getAction()
     {
@@ -65,56 +165,27 @@ class Mail_ImapController extends Zend_Controller_Action
 	    'host'     => 'mgate3.dm-mundschenk.de',
 	    'user'     => 'fleurop',
 	    'password' => 'fleurop1234'));
-       
+
         $filter = json_decode($this->getRequest()->getParam('filter', null));
         
-        if (empty($filter)) return;
+        // if (empty($filter)) return;
         
-        $partner_nr = reset($filter);
-        // if (empty($partner_nr)) $partner_nr = "99999999";
-        $partner_nr = $partner_nr->value;
-        if (empty($partner_nr)) return; // $this->getRequest()->getParam('partner_nr', "00000000");
-        $messages = $storage->search($partner_nr);
+        $property = reset($filter);
         
-        $data = array();
-        foreach ($messages as $messageid) {
-        	$datamsg = new stdClass();
-        	$msg = $storage->getMessage($messageid); 
-        	$datamsg->id = $messageid;
-        	$datamsg->mailFrom = iconv("ISO-8859-1", "UTF-8", quoted_printable_decode($msg->getHeaderField('from'))); 
-        	$datamsg->mailTo = iconv("ISO-8859-1", "UTF-8", quoted_printable_decode($msg->getHeaderField('to')));
-        	$datamsg->mailSubject = iconv("ISO-8859-1", "UTF-8", quoted_printable_decode($msg->getHeaderField('subject')));
-        	$datamsg->mailDate = date('Y-m-d H:i:s', strtotime($msg->getHeaderField('date')));
-        	$datamsg->mailMessage = "";
-			
-        	$foundPart = null;
-			foreach (new RecursiveIteratorIterator($msg) as $part) {
-			    try {
-			    	
-			        if (strtok($part->contentType, ';') == 'text/plain' || strtok($part->contentType, ';') == 'text/html') {
-			        	$foundPart = $part;
-			            break;
-			        }
-			    } catch (Zend_Mail_Exception $e) {
-			        // ignorieren
-			    }
-			}
-			if (empty($foundPart)) {
-				$datamsg->mailMessage = iconv("ISO-8859-1", "UTF-8", strip_tags(quoted_printable_decode($msg->getContent())));
-			} else {
-				$datamsg->mailMessage = iconv("ISO-8859-1", "UTF-8", strip_tags(quoted_printable_decode($foundPart)));
-			}
+        switch($property->property) {
+        	case 'partner_nr':
+        		$data = $this->getMails($storage, $filter);
+        		break;
+        	case 'id':
+        		$data = $this->getMail($storage, $property->value);
+        		break;
+        	default:
+        		$messageid = $this->getRequest()->getParam('id', null);
+        		if (!empty($messageid)) {
+        			$data = $this->getMail($storage, $messageid);
+        		}
+        		break;
         	
-				$testc = new stdClass();
-				$testc->test = utf8_decode($datamsg->mailMessage);
-			    $testc = json_decode(json_encode($testc));
-			    if (!empty($testc->test)) {
-			    	$datamsg->mailMessage = utf8_decode($datamsg->mailMessage);
-			    }
-
-			
-        	
-        	$data[] = $datamsg;
         }
         
         $this->view->data = $data;
